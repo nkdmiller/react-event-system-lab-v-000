c601ccb5c03df834afc947ebb41f1ca12817c5fb
Nate Miller <natekmiller96@gmail.com>
2018-11-02 18:48:24 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,6 +2,6 @@ import React, {Component} from 'react';
 
 
 export default class Keypad extends Component {
-  typing = () =>
+  typing = () => {console.log('Entering password...')};
   <input type="password" />
 }
